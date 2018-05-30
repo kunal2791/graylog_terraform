576ea57f40e213fbe03963d7abf0dd82e8e00732
kunal2791 <kunal2791@gmail.com>
2018-05-30 17:19:47 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -16,7 +16,6 @@ module "graylog" {
    ssh_key = "${var.ssh_key}"
    subnet_public_ids = ["${element(var.subnet_public_ids,1)}", "${element(var.subnet_public_ids,2)}"]
    vpc_id = "${var.vpc_id}"
-   #ssl_arn = "arn:aws:acm:eu-west-1:822536798293:certificate/5e78fc45-220e-4200-a018-c09aace375d0"
    r53zone_id = "${var.r53zone_id}"
    dns_name = "${var.dns_name}"
 }
